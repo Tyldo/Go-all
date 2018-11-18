74587f4f6ffe5379f832d73ddb0591998dbc1c49
Soslan <kunduhovsos@gmail.com>
2018-11-18 06:39:49 +0300

    Editing insertionSort.go

--- insertionSort.go
+++ insertionSort.go
@@ -9,10 +9,9 @@ func sort(arr *[]int) {
 	for i := 0; i < len(*arr)-1; i++ {
 		if (*arr)[i] > (*arr)[i+1] {
 			currentIndex := i + 1
-			nIndex := findIndex(*arr, (*arr)[currentIndex])
-			insertValue(arr, (*arr)[currentIndex], nIndex, currentIndex)
+			indexAt := findIndex(*arr, (*arr)[currentIndex])
+			insertValue(arr, (*arr)[currentIndex], indexAt, currentIndex)
 		}
-		fmt.Printf("Отсортированно до позиции %d: %v \n", i, *arr)
 	}
 }
 func findIndex(arr []int, item int) int {
@@ -35,9 +34,9 @@ func insertValue(arr *[]int, item int, indexAt int, indexFrom int) {
 func main() {
 	x := make([]int, 5)
 	for i := 0; i < 5; i++ {
-		x[i] = int(rand.Float32()*10)
+		x[i] = int(rand.Float32() * 10)
 	}
-  fmt.Println("До сортировки: %v", x)
+	fmt.Println("Unsorted array: ", x)
 	sort(&x)
-	fmt.Println("Результат: ", x)
+	fmt.Println("Sorted array: ", x)
 }
